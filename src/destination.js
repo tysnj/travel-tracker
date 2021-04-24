0959df7a28a6362291c33b0d69c606a4d7e70c61
Tyson McNutt <tyson.mcnutt@gmail.com>
2021-04-24 10:30:23 -0600

    Add destination class constructor

---
+++ src/destination.js
@@ -0,0 +1,10 @@
+export default class Destination {
+  constructor(id, destination, estimatedLodgingCostPerDay,estimatedFlightCostPerPerson, image, alt) {
+    this.id = id,
+    this.destination = destination,
+    this.estimatedLodgingCostPerDay = estimatedLodgingCostPerDay,
+    this.estimatedFlightCostPerPerson = estimatedFlightCostPerPerson,
+    this.image = image,
+    this.alt = alt
+  }
+}
