e0603e542adedb5e0914e1d5f51e78fbfe47cec6
Tyson McNutt <tyson.mcnutt@gmail.com>
2021-04-25 17:12:57 -0600

    Add image placeholder to layout

--- src/dom.js
+++ src/dom.js
@@ -3,12 +3,14 @@ const domUpdates = {
   populateTrips(data) {
     let tripsDisplay = document.getElementById("tripsDisplay")
     data.forEach(trip => {
-      tripsDisplay.insertAdjacentHTML("afterbegin",
-      `<div class="trip-container">
+      tripsDisplay.insertAdjacentHTML("beforeend",
+      `<div class="trip-container ${trip.status}">
       <img class="trip-image" src="${trip.destination.image}" alt="${trip.destination.alt}" hidden>
       ${trip.destination.destination}<br>
-      ${trip.startDate} - ${trip.endDate} (${trip.duration} days)<br>
-      ${trip.status}
+      ${trip.startDate} - ${trip.endDate}<br>
+      Duration: ${trip.duration} days<br>
+      Estimated Cost: $${trip.estCost.toLocaleString()}<br>
+      Status: ${trip.status}
       </div>
       `
       )
